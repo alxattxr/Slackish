85afcc78dd4465c844307ebef92ba8d753a030b5
Alexandre Attar <hello@alxndrattxr.com>
2020-01-04 15:18:23 -0500

    Login/Sign Up Page
    Added:
    - login page
    - sign up page
    - Auth using firebase
    - token generation(firebase)
    - token validation
    - ask notification permission
    
    Modified:
    - ...
    
    TODO:
    - display messages with notification when away from application page and when permission granted
    - Handle Errors (e.g.: invalid email, email in use, not same password, etc)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Login } from "./Components/Login/Login";
+import { Main } from "./Components/Main";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import firebase, { auth } from './firebase.config';
 
-function App() {
+const App = () => {
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState('');
+  const [verified, setVerified] = useState(false);
+  let currentUser;
+
+  useEffect(() => {
+    //listener
+    verifyToken();
+
+    if (typeof Notification !== "undefined") {
+      Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+          console.log("Notification permission granted.");
+          // TODO: Retrieve an Instance ID token for use with FCM.
+          // ...
+        } else {
+          console.log("Unable to get permission to notify.");
+        }
+      });
+    }
+
+    currentUser = firebase.auth().currentUser;
+    firebase.auth().onAuthStateChanged((newUser) => {
+      if (user === currentUser) {
+        setVerified(true);
+      } else if (newUser) {
+        setUser({
+          displayName: newUser.newUser,
+          email: newUser.email,
+          uid: newUser.uid
+        });
+        setVerified(true);
+        // ...
+      } else {
+        setVerified(false);
+        // User is signed out.
+        // ...
+      }
+    });
+
+  }, [currentUser]);
+
+  const verifyToken = () => {
+    if (user === currentUser) {
+      return setVerified(true);
+    }
+    return setVerified(false);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          {verified ?
+            <React.Fragment>
+            <Redirect to='/'/>
+            <Route path="/">
+              <Main user={user}/>
+            </Route>
+            </React.Fragment>
+            :
+            <React.Fragment>
+              <Redirect to='/login'/>
+              <Route path="/login">
+              <Login />
+              </Route>
+            </React.Fragment>
+          }
+        </Switch>
+      </Router>
     </div>
   );
 }
